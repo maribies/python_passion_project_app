c1e6ef094e425ca0a6e942072396764ceba886e0
Marissa <marissa.biesecker@gmail.com>
2020-08-28 21:29:03 +0100

    Add styling and update template

--- retail_app/views.py
+++ retail_app/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
+from .models import Business, Designer, Product
 
 # Create your views here.
 def index(request):
     """The home page for the retail app, Find and Seek"""
 
-    return render(request, "retail_app/index.html")
+    """Show all products, businesses, and designers."""
+    products = Product.objects.order_by("designer")
+    businesses = Business.objects.order_by("name")
+    designers = Designer.objects.order_by("name")
+    business_name = businesses[0].name
+
+    context = {
+        "products": products,
+        "businesses": businesses,
+        "designers": designers,
+        "name": business_name,
+    }
+
+    return render(request, "retail_app/index.html", context)
