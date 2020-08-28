f35eca933873311ecfa19617c1eab48cf0e00ebb
Marissa <marissa.biesecker@gmail.com>
2020-08-28 09:28:26 +0100

    Add initial setup for the homepage

--- retail_app/views.py
+++ retail_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    """The home page for the retail app, Find and Seek"""
+
+    return render(request, "retail_app/index.html")
