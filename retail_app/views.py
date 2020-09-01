ca310198e5c915dda19ad350812478d3b3a458d2
Marissa <marissa.biesecker@gmail.com>
2020-09-01 23:37:47 +0100

    Update styling for adjusted models, hover, responsivness

--- retail_app/views.py
+++ retail_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
-from .models import Business, Designer, Product
+from .models import Business, Designer, Product, ProductImage, ProductStock
 
 # Create your views here.
 def index(request):
@@ -12,11 +13,19 @@ def index(request):
     designers = Designer.objects.order_by("name")
     business_name = businesses[0].name
 
-    context = {
-        "products": products,
-        "businesses": businesses,
-        "designers": designers,
-        "name": business_name,
-    }
+    for product in products:
+        product.images = ProductImage.objects.filter(product=product.pk)
+        product.stock = ProductStock.objects.filter(product=product.pk)
+
+    try:
+        context = {
+            "products": products,
+            "businesses": businesses,
+            "designers": designers,
+            "name": business_name,
+        }
+
+    except businesses.DoesNotExist and designers.DoesNotExist:
+        raise Http404("Data does not exist")
 
     return render(request, "retail_app/index.html", context)
