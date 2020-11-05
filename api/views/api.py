d4b6581424cbf6c97c809cef3ae9e4a40b9cdc29
Marissa <marissa.biesecker@gmail.com>
2020-11-05 19:23:55 +0000

    Add pagination to api

--- api/views/api.py
+++ api/views/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from api.serializers import ProductSerializer
 from retail_app.models import Product
 import json
+from django.core.paginator import Paginator
 
 
 def get_data(request):
@@ -11,15 +12,23 @@ def get_data(request):
         if request is None:
             raise Exception("Houston, we have a problem.")
 
-        search = request.GET.get("search")
+        # search = request.GET.get("search")
         page = request.GET.get("page")
         per_page = request.GET.get("per_page")
 
-        print("options", search, page, per_page)
-
         products = Product.objects.fully_loaded_objects()
+        products_for_serialization = products
+
+        if page is not None and per_page is not None:
+            paginator = Paginator(products, per_page)
+            products_for_serialization = paginator.get_page(page)
+
+            if len(products) / int(per_page) < int(page) or int(page) == 0:
+                products_for_serialization = []
+
         serializedProducts = [
-            ProductSerializer(product).to_json() for product in products
+            ProductSerializer(product).to_json()
+            for product in products_for_serialization
         ]
 
         products_data = {"products": serializedProducts}
--- python_passion_project/tests/test_views/test_api.py
+++ python_passion_project/tests/test_views/test_api.py
@@ -21,3 +21,44 @@ class TestApiView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(content, '{"products": []}')
         self.assertEqual(len(products), 20)
+
+    def test_get_data_pagination_one(self):
+        response = self.client.get(
+            "/api/v1/products/",
+            {"search": "", "page": 1, "per_page": 10},
+        )
+
+        content = response.content.decode("utf-8")
+
+        products = json.loads(content)["products"]
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(content, '{"products": []}')
+        self.assertEqual(len(products), 10)
+
+    def test_get_data_pagination_beyond_end(self):
+        response = self.client.get(
+            "/api/v1/products/",
+            {"search": "", "page": 3, "per_page": 10},
+        )
+
+        content = response.content.decode("utf-8")
+
+        products = json.loads(content)["products"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content, '{"products": []}')
+        self.assertEqual(len(products), 0)
+
+    def test_get_data_pagination_beyond_beginning(self):
+        response = self.client.get(
+            "/api/v1/products/",
+            {"search": "", "page": 0, "per_page": 10},
+        )
+
+        content = response.content.decode("utf-8")
+
+        products = json.loads(content)["products"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(content, '{"products": []}')
+        self.assertEqual(len(products), 0)
